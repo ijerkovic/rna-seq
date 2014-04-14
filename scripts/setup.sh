c61f22b0bdc1124540e6f3c8aff9db7e9c780e70
Igor Jerkovic <igor.jerkovich@gmail.com>
2014-04-14 02:07:06 +0200

    Updated setup for dependants such as gflags and divsufsort

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 export PATH=$PATH:$PWD/../lisa/
 export PATH=$PATH:$PWD/../rna-seq/
+
+# dependants are gflags and divsufsort
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/ijerkovic/gflags/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/ijerkovic/divsufsort/lib
